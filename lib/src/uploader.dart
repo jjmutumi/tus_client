50e8fd8913b0c00fc546322096c3b236ae4a0c5c
Joseph Mutumi <jjmutumi@gmail.com>
2020-09-02 19:02:34 +0300

    Calling flush early

--- lib/src/uploader.dart
+++ lib/src/uploader.dart
@@ -94,6 +94,7 @@ class TusUploader {
     final bytesRead = await upload.readInto(buffer, offset);
     // Do not write the entire buffer to the stream
     _httpRequest.add(buffer.sublist(0, bytesRead));
+    _httpRequest.flush();
 
     offset += bytesRead;
     _bytesRemainingForRequest -= bytesRead;

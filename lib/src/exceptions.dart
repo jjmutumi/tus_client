9145a7ca2c860a21f9df1e37dcd9576f1375e7c7
Joseph Mutumi <jjmutumi@gmail.com>
2021-02-04 10:02:30 +0300

    Removing unused exceptions (deadcode)

--- lib/src/exceptions.dart
+++ lib/src/exceptions.dart
@@ -9,23 +9,3 @@ class ProtocolException implements Exception {
 
   String toString() => "ProtocolException: $message";
 }
-
-/// This exception is thrown by [TusClient.resumeUpload] if no upload Uri
-/// has been stored in the [TusClient.store]
-class FingerprintNotFoundException implements Exception {
-  final String fingerprint;
-
-  FingerprintNotFoundException(this.fingerprint);
-
-  String toString() => "FingerprintNotFoundException: "
-      "fingerprint not in storage found: $fingerprint";
-}
-
-/// This exception is thrown when you try to resume an upload using
-/// [TusClient.resumeUpload] without configuring a [TusClient.store].
-class ResumingNotEnabledException implements Exception {
-  ResumingNotEnabledException();
-
-  String toString() => "ResumingNotEnabledException: "
-      "resuming not enabled for this client, set a store to do so";
-}
--- test/src/exceptions_test.dart
+++ test/src/exceptions_test.dart
@@ -2,22 +2,6 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:tus_client/tus_client.dart';
 
 main() {
-  test("exceptions_test.FingerprintNotFoundException", () {
-    final err = FingerprintNotFoundException("test");
-    expect(
-        "$err",
-        "FingerprintNotFoundException: "
-            "fingerprint not in storage found: test");
-  });
-
-  test("exceptions_test.ResumingNotEnabledException", () {
-    final err = ResumingNotEnabledException();
-    expect(
-        "$err",
-        "ResumingNotEnabledException: "
-            "resuming not enabled for this client, set a store to do so");
-  });
-
   test("exceptions_test.ProtocolException", () {
     final err = ProtocolException("Expected HEADER 'Tus-Resumable'");
     expect(
